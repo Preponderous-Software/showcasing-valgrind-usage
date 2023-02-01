b3ef01549bfcac70f003ba09aa2750e2967d8f39
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-02-01 10:43:34 -0700

    Created Leaker class.

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,17 +1,25 @@
 #include <string>
 #include <iostream>
 
-int main() {
-    int numStrings = 10000;
-    std::cout << "Creating " << numStrings << " strings..." << std::endl;
-    // cause a memory leak
-    for (int i = 0; i < 10000; i++) {
-        std::string* s = new std::string("string #" + std::to_string(i));
-        std::cout << *s << std::endl;
+class Leaker {
+    public:
+        void createStrings() {
+            int numStrings = 10000;
+            std::cout << "Creating " << numStrings << " strings..." << std::endl;
+            // cause a memory leak
+            for (int i = 0; i < 10000; i++) {
+                std::string* s = new std::string("string #" + std::to_string(i));
+                std::cout << *s << std::endl;
 
-        // uncomment this line to fix the memory leak
-        // delete s;
-    }
-    std::cout << numStrings << " strings created!" << std::endl;
+                // uncomment this line to fix the memory leak
+                // delete s;
+            }
+            std::cout << numStrings << " strings created!" << std::endl;
+        }
+};
+
+int main() {
+    Leaker leaker;
+    leaker.createStrings();
     return 0;
 }
